01985399e66e0e07dfc8b43a10f6d457a9f531f4
Baris Burak Balci <barisburakbalci@gmail.com>
2024-01-22 16:45:39 +0300

    Created a server health check

--- lib/page_template.dart
+++ lib/page_template.dart
@@ -1,9 +1,13 @@
+import 'dart:developer';
+
 import 'package:cocoforms/pages/answers_page.dart';
 import 'package:cocoforms/pages/customers_page.dart';
 import 'package:cocoforms/pages/forms_page.dart';
 import 'package:cocoforms/pages/profile_page.dart';
 import 'package:cocoforms/widgets/bottom_navbar.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:cocoforms/variables.dart' as vars;
 
 class PageTemplate extends StatefulWidget {
   const PageTemplate({super.key});
@@ -14,6 +18,7 @@ class PageTemplate extends StatefulWidget {
 
 class _PageTemplateState extends State<PageTemplate> {
   int _currentPage = 0;
+  bool isServerHealthy = false;
 
   final List<Widget> _pages = <Widget>[
     const FormsPage(),
@@ -28,19 +33,39 @@ class _PageTemplateState extends State<PageTemplate> {
     });
   }
 
+  Future<bool> pingServer() async {
+    var response = await http.get(Uri.parse('${vars.baseUrl}Form'));
+    return response.body.isNotEmpty;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    pingServer().then((value) => setState(() => isServerHealthy = value));
+  }
+
   @override
   Widget build(BuildContext context) {
     try {
-      return Scaffold(
-        body: IndexedStack(
-          index: _currentPage,
-          children: _pages,
-        ),
-        bottomNavigationBar: BottomNavbar(
-          currentPage: _currentPage,
-          onItemTapped: _onItemTapped,
-        ),
-      );
+      return isServerHealthy
+          ? Scaffold(
+              body: IndexedStack(
+                index: _currentPage,
+                children: _pages,
+              ),
+              bottomNavigationBar: BottomNavbar(
+                currentPage: _currentPage,
+                onItemTapped: _onItemTapped,
+              ),
+            )
+          : const Scaffold(
+              body: Center(
+                child: Text(
+                  'Sunucu hatası!',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
+            );
     } catch (e) {
       return Center(
         child: Text(e.toString()),
