11bd3883e7c690d82d8bfe89d933c2df3937c3a6
Baris Burak Balci <barisburakbalci@gmail.com>
2024-05-13 04:44:02 +0300

    App should bypass login when there is a user

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:cocoforms/core/services/global_providers.dart';
+import 'package:cocoforms/features/auth/services/auth_service.dart';
 import 'package:cocoforms/features/form_view/screens/forms_screen.dart';
 import 'package:cocoforms/features/auth/screens/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const Ticform());
@@ -13,16 +15,24 @@ class Ticform extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GlobalProviders(
-      child: MaterialApp(
-        title: 'Ticform',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        routes: {
-          '/login': (context) => const LoginScreen(),
-          '/forms': (context) => const FormsScreen(),
+      child: Consumer<AuthService>(
+        builder: (_, authService, __) {
+          String initialRoute = '/login';
+          if (authService.user.isNotEmpty) {
+            initialRoute = '/forms';
+          }
+          return MaterialApp(
+            title: 'Ticform',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+            ),
+            routes: {
+              '/login': (context) => const LoginScreen(),
+              '/forms': (context) => const FormsScreen(),
+            },
+            initialRoute: initialRoute,
+          );
         },
-        initialRoute: '/login',
       ),
     );
   }
